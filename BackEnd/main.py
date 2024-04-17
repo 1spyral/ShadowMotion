d9d431652ddc9cd573b93fc9b87cc0d8d2494613
1spyral <83098914+1spyral@users.noreply.github.com>
2024-04-17 19:52:31 -0400

    Update main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,4 +1,14 @@
+import server
+import client
 
+
+def main():
+    
+    clients = []
+    server = server.Server(clients)
+
+    # TODO: loop server.update()
+    # TODO: process client messages
 
 if __name__ == "__main__":
-    pass
+    main()
