0232e0ba5b97615674b31a76dd6132abccf71457
1spyral <83098914+1spyral@users.noreply.github.com>
2024-04-17 20:26:44 -0400

    add server loop to main

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,14 +1,18 @@
-import server
+import server as s
 import client
 
 
 def main():
     
     clients = []
-    server = server.Server(clients)
-
-    # TODO: loop server.update()
-    # TODO: process client messages
+    server = s.Server(clients)
+    
+    try:
+        while True:
+            server.update()
+            # TODO: process client messages
+    except KeyboardInterrupt:
+        server.close()
 
 if __name__ == "__main__":
     main()
